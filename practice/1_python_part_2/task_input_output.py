8e1dfd9db663033302a8f2da0ec066bd6d09d640
vpopov <vpopov@griddynamics.com>
2021-09-29 15:52:44 +0300

    Part 2 task 1

---
+++ practice/1_python_part_2/task_input_output.py
@@ -0,0 +1,23 @@
+"""
+Write function which reads a number from input nth times.
+If an entered value isn't a number, ignore it.
+After all inputs are entered, calculate an average entered number.
+Return string with following format:
+If average exists, return: "Avg: X", where X is avg value which rounded to 2 places after the decimal
+If it doesn't exists, return: "No numbers entered"
+Examples:
+    user enters: 1, 2, hello, 2, world
+    >>> read_numbers(5)
+    Avg: 1.67
+    ------------
+    user enters: hello, world, foo, bar, baz
+    >>> read_numbers(5)
+    No numbers entered
+
+"""
+
+
+def read_numbers(n: int) -> str:
+    ...
+
+
