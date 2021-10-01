b6a1b88654640bd3787c4a6aee62f824433c7990
vpopov <vpopov@griddynamics.com>
2021-10-01 13:04:39 +0300

    Part 2 task 3

---
+++ practice/1_python_part_2/test_task_classes.py
@@ -0,0 +1,5 @@
+"""
+Write tests for classes you write during task (Homework, Teacher, Student)
+Check if all methods working correctly
+Also check corner-cases, for example if homework number of days is negative,
+"""
