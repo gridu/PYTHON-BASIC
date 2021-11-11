2b33e394691f2512297853e60354429e4d5a75cb
Andrey Kot <kotovskyandrew@yandex.ru>
2021-11-11 12:43:44 +0300

    add testing part and move other parts

--- practice/2_python_part_2/test_task_classes.py
+++
@@ -1,5 +0,0 @@
-"""
-Write tests for classes you write during task (Homework, Teacher, Student)
-Check if all methods working correctly
-Also check corner-cases, for example if homework number of days is negative,
-"""
---
+++ practice/3_python_testing/test_task_classes.py
@@ -0,0 +1,5 @@
+"""
+Write tests for classes in 2_python_part_2/task_classes.py (Homework, Teacher, Student).
+Check if all methods working correctly.
+Also check corner-cases, for example if homework number of days is negative.
+"""
