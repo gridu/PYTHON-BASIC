31a1e1cef7028ef2372fac41252131ce56a97714
Andrey Kot <kotovskyandrew@yandex.ru>
2021-11-15 13:51:06 +0300

    add new tasks for read-write topic

---
+++ practice/2_python_part_2/task_read_write.py
@@ -0,0 +1,15 @@
+"""
+Read files from ./files and extract values from them.
+Write one file with all values separated by commas.
+
+Example:
+    Input:
+
+    file_1.txt (content: "23")
+    file_2.txt (content: "78")
+    file_3.txt (content: "3")
+
+    Output:
+
+    result.txt(content: "23, 78, 3")
+"""
---
+++ practice/3_python_testing/test_read_write.py
@@ -0,0 +1,6 @@
+"""
+Write tests for 2_python_part_2/task_read_write.py task.
+You temporary files for writing files during tests:
+https://docs.python.org/3/library/tempfile.html
+https://docs.pytest.org/en/6.2.x/tmpdir.html
+"""
