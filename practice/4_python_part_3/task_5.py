c3277d9ed140868ca6905ca929674767200daf7f
bustianovskyi <bustianovskyi@griddynamics.com>
2021-11-17 17:54:13 +0200

    add task 5 - making requests to internet using urllib

---
+++ practice/4_python_part_3/task_5.py
@@ -0,0 +1,26 @@
+"""
+Write a function that makes a request to some url
+using urllib. Return status code and decoded response data in utf-8
+Examples:
+     >>> make_request('https://www.google.com')
+     200, 'response data'
+"""
+from typing import Tuple
+
+
+def make_request(url: str) -> Tuple[int, str]:
+    ...
+
+
+"""
+Write test for make_request function
+Use Mock for mocking request with urlopen https://docs.python.org/3/library/unittest.mock.html#unittest.mock.Mock
+Example:
+    >>> m = Mock()
+    >>> m.method.return_value = 200
+    >>> m.method2.return_value = b'some text'
+    >>> m.method()
+    200
+    >>> m.method2()
+    b'some text'
+"""
