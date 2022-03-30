33a33d95d87889ceb7963856b1e350ee6c9d880d
kkolodziejska <kkolodziejska@griddynamics.com>
2022-03-30 14:15:20 +0200

    task 2 done

--- practice/7_concurrency/task2_apod_api/template.py
+++ practice/7_concurrency/task2_apod_api/template.py
@@ -1,14 +1,50 @@
-API_KEY = "YOUR_KEY"
+import requests
+import json
+from os import path
+import os
+import threading
+import concurrent.futures
+
+
+API_KEY = "API_KEY"  # GET YOUR OWN API KEY
 APOD_ENDPOINT = 'https://api.nasa.gov/planetary/apod'
 OUTPUT_IMAGES = './output'
 
+thread_local = threading.local()
+
+
+def get_session() -> requests.Session:
+    if not hasattr(thread_local, 'session'):
+        thread_local.session = requests.Session()
+    return thread_local.session
+
 
 def get_apod_metadata(start_date: str, end_date: str, api_key: str) -> list:
-    pass
+    response = requests.get(APOD_ENDPOINT, params={'start_date': start_date,
+                                                   'end_date': end_date,
+                                                   'api_key': api_key})
+    metadata = json.loads(response.text)
+    return metadata
 
 
-def download_apod_images(metadata: list):
-    pass
+def download_apod_images(metadata: list) -> None:
+    if not os.path.exists(OUTPUT_IMAGES):
+        os.mkdir(OUTPUT_IMAGES)
+    images_urls = [item['url'] for item in metadata
+                   if item['media_type'] != 'video']
+    with concurrent.futures.ThreadPoolExecutor(max_workers=5) as executor:
+        executor.map(download_image, images_urls)
+
+
+def download_image(url: str) -> None:
+    filename = url.split('/')[-1]
+    filepath = path.join(OUTPUT_IMAGES, filename)
+    s = get_session()
+    with s.get(url, stream=True) as response:
+        if response.status_code == 200:
+            with open(filepath, 'wb') as f:
+                for chunk in response:
+                    f.write(chunk)
 
 
 def main():
