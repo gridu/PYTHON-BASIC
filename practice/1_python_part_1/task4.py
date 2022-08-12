7c3109bcc48e43499bf9099b61a61711252249e5
odruzhinin <odruzhinin>
2022-08-12 23:02:55 +0200

    Task4 resolved

--- practice/1_python_part_1/task4.py
+++ practice/1_python_part_1/task4.py
@@ -10,4 +10,9 @@ from typing import List
 
 
 def calculate_power_with_difference(ints: List[int]) -> List[int]:
-    ...
+    result=[]
+    prev_int=0
+    for i in ints:
+        result.append(i**2-(prev_int**2-prev_int))
+        prev_int=i
+    return result
