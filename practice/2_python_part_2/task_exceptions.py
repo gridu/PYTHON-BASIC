86e721d430b9003717b6988a2b15e3bc5c1047aa
wiktormorski <104913406+wiktormorski@users.noreply.github.com>
2022-08-26 11:57:50 +0200

    correct a semantic mistake

--- practice/2_python_part_2/task_exceptions.py
+++ practice/2_python_part_2/task_exceptions.py
@@ -1,7 +1,7 @@
 """
 Write a function which divides x by y.
 If y == 0 it should print "Division by 0" and return None
-elif y == 1 it should raise custom Exception with "Deletion on 1 get the same result" text
+elif y == 1 it should raise custom Exception with "Dividing by 1 gives the same result" text
 else it should return the result of division
 In all cases it should print "Division finished"
     >>> division(1, 0)
@@ -9,7 +9,7 @@ In all cases it should print "Division finished"
     Division finished
     >>> division(1, 1)
     Division finished
-    DivisionByOneException("Deletion on 1 get the same result")
+    DivisionByOneException("Dividing by 1 gives the same result")
     >>> division(2, 2)
     1
     Division finished
