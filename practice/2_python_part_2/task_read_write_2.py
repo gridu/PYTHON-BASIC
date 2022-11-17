ffeb4e4f534f104c783ae97ce8389880c46863ab
imacedonski <i.macedonski@gmail.com>
2022-11-17 10:04:15 +0100

    .

--- practice/2_python_part_2/task_read_write_2.py
+++ practice/2_python_part_2/task_read_write_2.py
@@ -20,5 +20,20 @@ def generate_words(n=20):
     for _ in range(n):
         word = ''.join(random.choices(string.ascii_lowercase, k=random.randint(3, 10)))
         words.append(word)
-
+    with open('./files/file1.txt', 'w', encoding='utf-8') as f:
+        for number, w in enumerate(words):
+            if number != n - 1:
+                f.write(w + '\n')
+            else:
+                f.write(w)
+    with open('./files/file2.txt', 'w', encoding='cp1252') as g:
+        for number, w in enumerate(words[::-1]):
+            if number != n - 1:
+                g.write(w + ',')
+            else:
+                g.write(w)
     return words
+
+
+if __name__ == '__main__':
+    generate_words()
