eb01324ee2785f7b233b90e50a973835d111d31e
Karen Bocardo <karenbocardo@Karens-MBP.localdomain>
2022-12-06 15:12:20 -0600

    part 1: task 4

--- practice/1_python_part_1/task4.py
+++ practice/1_python_part_1/task4.py
@@ -10,4 +10,12 @@ from typing import List
 
 
 def calculate_power_with_difference(ints: List[int]) -> List[int]:
-    ...
+    #out = [ints[0]**2]
+    #for i, x in enumerate(ints[1:]):
+    #    prev = ints[i]
+    #    out.append(x**2 - (prev**2 - prev))
+    # return out
+
+    power = [x ** 2 for x in ints]
+    difference = [x - (power[i] - ints[i]) for i, x in enumerate(power[1:])]
+    return [power[0]] + difference
