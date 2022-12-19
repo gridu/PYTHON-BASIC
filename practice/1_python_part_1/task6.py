30c299ec536644b4eed8ca6e239a7b5cdd7e8956
Wojtek <119590156+wkowalczykGrid@users.noreply.github.com>
2022-12-19 10:25:19 +0100

    Update task6.py

--- practice/1_python_part_1/task6.py
+++ practice/1_python_part_1/task6.py
@@ -20,14 +20,10 @@ from typing import Tuple
 
 
 def get_min_max(filename: str) -> Tuple[int, int]:
-    counter = 0
-    total = 0
     num_list = []
     with open(filename) as opened_file:
         for num in opened_file.read().split():
             num_list.append(float(num))
-            counter = len(num_list)
-            total = sum(num_list)
             mn = min(num_list)
             mx = max(num_list)
         print(mn, mx)
