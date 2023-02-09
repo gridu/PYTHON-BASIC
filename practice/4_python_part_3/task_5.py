763e05755ab70b4bb985067645a74a20fa76a74c
jtelesinska <jtelesinska@griddynamics.com>
2023-02-09 12:19:20 +0100

    completed function for task5 part4

--- practice/4_python_part_3/task_5.py
+++ practice/4_python_part_3/task_5.py
@@ -6,12 +6,19 @@ Examples:
      200, 'response data'
 """
 from typing import Tuple
-
+from urllib.request import urlopen
+from urllib.error import HTTPError
+import ssl
 
 def make_request(url: str) -> Tuple[int, str]:
-    ...
-
-
+    ssl._create_default_https_context = ssl._create_unverified_context
+    try:
+        with urlopen(url) as response:
+            status_code = response.getcode()
+            response_data = response.read().decode('utf-8')
+            return status_code, response_data
+    except HTTPError as e:
+        return e.getcode(), e.read().decode('utf-8')
 """
 Write test for make_request function
 Use Mock for mocking request with urlopen https://docs.python.org/3/library/unittest.mock.html#unittest.mock.Mock
