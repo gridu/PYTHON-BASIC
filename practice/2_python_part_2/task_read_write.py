5a8ff4c6d119a8f8e39418b21a10a00f4a5217ec
tsalmon2 <tsalmon@griddynamics.com>
2023-03-02 08:34:43 -0500

    Completed task_read_write

--- practice/2_python_part_2/task_read_write.py
+++ practice/2_python_part_2/task_read_write.py
@@ -13,3 +13,24 @@ Example:
 
     result.txt(content: "23, 78, 3")
 """
+
+import os
+
+def read_files(*files):
+    path = r"\files"
+
+    print(files)
+    # Need to cd into folder above files
+    path_to_folder = os.getcwd() + path
+    # files_list = [file for sub_dir, dirs, files in os.walk(path_to_folder) for file in files]
+    files_list = [file for file in files]
+    print(files_list)
+    res_lst = []
+
+    for file in files_list:
+        with open(f"{path_to_folder}\{file}", "r") as f:
+            file_content=f.read()
+            res_lst.append(file_content)
+
+    with open(fr"{path_to_folder}\result.txt", "w") as r:
+        r.write(",".join(res_lst))
