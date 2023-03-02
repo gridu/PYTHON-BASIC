b2fd029df674d42a8b3ef0c450d9721d77f1637a
tsalmon2 <tsalmon@griddynamics.com>
2023-03-02 07:43:07 -0500

    Completed task4

--- practice/1_python_part_1/task4.py
+++ practice/1_python_part_1/task4.py
@@ -6,8 +6,9 @@ Examples:
     >>> calculate_power_with_difference([1, 2, 3])
     [1, 4, 7]  # because [1^2, 2^2 - (1^2 - 1), 3^2 - (2^2 - 2)]
 """
+
 from typing import List
 
-
 def calculate_power_with_difference(ints: List[int]) -> List[int]:
-    ...
+    final_lst = [num**2 - (ints[ind-1]**2 - ints[ind-1]) if ind != 0 else num**2 for ind, num in enumerate(ints)]
+    return final_lst
