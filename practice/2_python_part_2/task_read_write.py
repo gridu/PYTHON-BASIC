1a7baa94cfad01e73bdcda03c8d5db73a1d1043e
tsalmon2 <tsalmon@griddynamics.com>
2023-03-17 09:33:13 -0500

    Updated task_read_write to take result file path

--- practice/2_python_part_2/task_read_write.py
+++ practice/2_python_part_2/task_read_write.py
@@ -16,7 +16,7 @@ Example:
 
 import os
 
-def read_files(*files):
+def read_files(*files, res_file_path):
     path = r"/files"
 
     # Need to cd into folder above files
@@ -29,13 +29,15 @@ def read_files(*files):
 
     # Add contents of each file to results list
     for file in files_list:
-        with open(f"{path_to_folder}/{file}", "r", encoding="utf-8") as f:
-            file_content=f.read().splitlines() # Reading all lines of file in case content is on multiple lines
-            res_lst.extend(file_content)
-
+        try:
+            with open(f"{path_to_folder}/{file}", "r", encoding="utf-8") as f:
+                file_content=f.read().splitlines() # Reading all lines of file in case content is on multiple lines
+                res_lst.extend(file_content)
+        except FileNotFoundError:
+            raise FileNotFoundError("File doesn't exist. Please recheck file names.")
     # Output the contents of each file to the result text file separated by a comma and space
-    with open(fr"{path_to_folder}/result.txt", "w", encoding="utf-8") as r:
+    with open(res_file_path, "w", encoding="utf-8") as r:
         r.write(", ".join(res_lst))
 
 if __name__ == '__main__':
-    read_files("file_1.txt", "file_2.txt", "file_3.txt")
+    read_files("file_1.txt", "file_2.txt", "file_3.txt", res_file_path="/Users/tiasalmon/Documents/Python_Basics/python_basic/practice/2_python_part_2/files/result.txt")
