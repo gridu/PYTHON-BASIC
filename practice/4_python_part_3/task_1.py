0302ace76cea31bf754649f1d2cc0398630eb75d
tsalmon2 <tsalmon@griddynamics.com>
2023-03-17 11:08:54 -0500

    Completed task_1

--- practice/4_python_part_3/task_1.py
+++ practice/4_python_part_3/task_1.py
@@ -12,9 +12,22 @@ If entered date is from future, return negative value for number of days
 """
 from datetime import datetime
 
+class WrongFormatException(Exception):
+    pass
 
 def calculate_days(from_date: str) -> int:
-    ...
+    date_format = '%Y-%m-%d'
+    current_date = datetime.strptime(datetime.today().strftime(date_format), date_format)
+    try:
+        from_date = datetime.strptime(from_date, date_format)
+    except ValueError:
+        raise WrongFormatException(f"{from_date} format is invalid. Format should match '{date_format}'")
+    return (current_date - from_date).days
+
+if __name__ == "__main__":
+    print(calculate_days('2023-03-18'))
+    print(calculate_days('2023-03-02'))
+    # calculate_days('10-07-2021')
 
 
 """
