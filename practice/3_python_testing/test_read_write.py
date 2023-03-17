fc280ef0ee1fbdd7664429f9aa83e0ed10cf0bef
tsalmon2 <tsalmon@griddynamics.com>
2023-03-17 09:27:33 -0500

    Completed test_read_write

--- practice/3_python_testing/test_read_write.py
+++ practice/3_python_testing/test_read_write.py
@@ -4,3 +4,33 @@ To write files during tests use temporary files:
 https://docs.python.org/3/library/tempfile.html
 https://docs.pytest.org/en/6.2.x/tmpdir.html
 """
+
+# To allow importing modules from other directories
+import sys
+import unittest
+import tempfile
+import os
+
+# Allowing file to search for modules in parent directory. Need to cd into 3_python_testing folder
+sys.path.append("..")
+
+from python_part_2 import task_read_write as task
+
+class Read_Write_Tests(unittest.TestCase):
+    def test_read_write_output(self):
+        temp_path = tempfile.mkstemp()[1]
+        task.read_files("file_1.txt", "file_2.txt", res_file_path=temp_path)
+        with open(temp_path, "r", encoding="utf-8") as temp_file:
+            contents = temp_file.read()
+        os.remove(temp_path)
+        self.assertEqual(contents, "80, 37")
+
+    def test_invalid_filename(self):    
+        temp_path = tempfile.mkstemp()[1]
+        with self.assertRaises(FileNotFoundError):
+            task.read_files("file_1.txt", "files_2.txt", res_file_path=temp_path)
+        os.remove(temp_path)
+
+
+if __name__ == '__main__':
+    unittest.main()
