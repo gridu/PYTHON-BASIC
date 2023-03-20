aaa139f000c73ee1b4ee555934a798ad0a6a1ecb
tsalmon2 <tsalmon@griddynamics.com>
2023-03-20 09:49:45 -0500

    Changed testing framework to pytest

--- practice/3_python_testing/test_read_write.py
+++ practice/3_python_testing/test_read_write.py
@@ -7,8 +7,8 @@ https://docs.pytest.org/en/6.2.x/tmpdir.html
 
 # To allow importing modules from other directories
 import sys
-import unittest
 import tempfile
+import pytest
 import os
 
 # Allowing file to search for modules in parent directory. Need to cd into 3_python_testing folder
@@ -16,21 +16,17 @@ sys.path.append("..")
 
 from python_part_2 import task_read_write as task
 
-class Read_Write_Tests(unittest.TestCase):
-    def test_read_write_output(self):
-        temp_path = tempfile.mkstemp()[1]
-        task.read_files("file_1.txt", "file_2.txt", res_file_path=temp_path)
-        with open(temp_path, "r", encoding="utf-8") as temp_file:
-            contents = temp_file.read()
-        os.remove(temp_path)
-        self.assertEqual(contents, "80, 37")
+# Need to cd into 3_python_testing
+def test_read_write_output():
+    temp_path = tempfile.mkstemp()[1]
+    task.read_files("file_1.txt", "file_2.txt", res_file_path=temp_path)
+    with open(temp_path, "r", encoding="utf-8") as temp_file:
+        contents = temp_file.read()
+    os.remove(temp_path)
+    assert contents == "80, 37"
 
-    def test_invalid_filename(self):    
-        temp_path = tempfile.mkstemp()[1]
-        with self.assertRaises(FileNotFoundError):
-            task.read_files("file_1.txt", "files_2.txt", res_file_path=temp_path)
-        os.remove(temp_path)
-
-
-if __name__ == '__main__':
-    unittest.main()
+def test_invalid_filename():    
+    temp_path = tempfile.mkstemp()[1]
+    with pytest.raises(FileNotFoundError):
+        task.read_files("file_1.txt", "files_2.txt", res_file_path=temp_path)
+    os.remove(temp_path)
--- practice/3_python_testing/test_read_write_2.py
+++ practice/3_python_testing/test_read_write_2.py
@@ -8,27 +8,23 @@ https://docs.pytest.org/en/6.2.x/tmpdir.html
 
 # To allow importing modules from other directories
 import sys
-import unittest
+import pytest
 import tempfile
 import os
 
 from python_part_2 import task_read_write_2 as task
 
 # Testing that the contents of the reversed file are in reverse order to the contents of the original file.
-class Read_Write_Tests_2(unittest.TestCase):
-    def test_read_write_2_reverse(self):
-        temp_rand_file = tempfile.mkstemp()[1]
-        temp_reverse_file = tempfile.mkstemp()[1]
-        task.task_read_write_2(temp_rand_file, temp_reverse_file)
-        with open(temp_rand_file, "r", encoding="utf-8") as temp_file:
-            contents = temp_file.readlines()
-        
-        with open(temp_reverse_file, "r", encoding="utf-8") as reversed_temp_file:
-            reversed_contents = reversed_temp_file.readlines()
-        self.assertEqual(reversed_contents, list(reversed(contents)))
+def test_read_write_2_reverse():
+    temp_rand_file = tempfile.mkstemp()[1]
+    temp_reverse_file = tempfile.mkstemp()[1]
+    task.task_read_write_2(temp_rand_file, temp_reverse_file)
+    with open(temp_rand_file, "r", encoding="utf-8") as temp_file:
+        contents = temp_file.readlines()
+    
+    with open(temp_reverse_file, "r", encoding="utf-8") as reversed_temp_file:
+        reversed_contents = reversed_temp_file.readlines()
+    assert reversed_contents == list(reversed(contents))
 
-        os.remove(temp_rand_file)
-        os.remove(temp_reverse_file)
-
-if __name__ == "__main__":
-    unittest.main()
+    os.remove(temp_rand_file)
+    os.remove(temp_reverse_file)
