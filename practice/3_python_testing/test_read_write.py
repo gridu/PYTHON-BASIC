0c49c69bbb75c45eaf262476f0ad243cccfd01f5
tsalmon2 <tsalmon@griddynamics.com>
2023-03-20 10:33:43 -0500

    Updated test_read_write to have tests within a class

--- practice/3_python_testing/test_read_write.py
+++ practice/3_python_testing/test_read_write.py
@@ -16,17 +16,18 @@ sys.path.append("..")
 
 from python_part_2 import task_read_write as task
 
-# Need to cd into 3_python_testing
-def test_read_write_output():
-    temp_path = tempfile.mkstemp()[1]
-    task.read_files("file_1.txt", "file_2.txt", res_file_path=temp_path)
-    with open(temp_path, "r", encoding="utf-8") as temp_file:
-        contents = temp_file.read()
-    os.remove(temp_path)
-    assert contents == "80, 37"
+class TestReadWrite:
+    # Need to cd into 3_python_testing
+    def test_read_write_output(self):
+        temp_path = tempfile.mkstemp()[1]
+        task.read_files("file_1.txt", "file_2.txt", res_file_path=temp_path)
+        with open(temp_path, "r", encoding="utf-8") as temp_file:
+            contents = temp_file.read()
+        os.remove(temp_path)
+        assert contents == "80, 37"
 
-def test_invalid_filename():    
-    temp_path = tempfile.mkstemp()[1]
-    with pytest.raises(FileNotFoundError, match=r"File doesn't exist. Please recheck file names."):
-        task.read_files("file_1.txt", "files_2.txt", res_file_path=temp_path)
-    os.remove(temp_path)
+    def test_invalid_filename(self):    
+        temp_path = tempfile.mkstemp()[1]
+        with pytest.raises(FileNotFoundError, match=r"File doesn't exist. Please recheck file names."):
+            task.read_files("file_1.txt", "files_2.txt", res_file_path=temp_path)
+        os.remove(temp_path)
