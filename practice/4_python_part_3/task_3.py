0aaa2e5325aa5aac33582685ee19fde04a341b16
tsalmon2 <tsalmon@griddynamics.com>
2023-03-22 12:57:38 -0500

    Updated task_3

--- practice/4_python_part_3/task_3.py
+++ practice/4_python_part_3/task_3.py
@@ -10,17 +10,23 @@ Note that address may have several domain levels
     False
 """
 import re
-
+import pytest
 
 def is_http_domain(domain: str) -> bool:
     pattern = r"^((http|https):\/\/)(([\w]-?)+\w\.\w+)(([\w]-?)+\w\.\w+)?\/?$"
     return bool(re.match(pattern, domain))
 
-if __name__ == "__main__":
-    print(is_http_domain('http://wikipedia.org'))
-    print(is_http_domain('https://ru.wikipedia.org/'))
-    print(is_http_domain('griddynamics.com'))
-
 """
 write tests for is_http_domain function
 """
+
+
+class TestHTTPDomain:
+    def test_valid_domain_http(self):
+        assert is_http_domain('http://wikipedia.org')
+    
+    def test_valid_domain_https(self):
+        assert is_http_domain('https://ru.wikipedia.org/')
+    
+    def test_invalid_domain(self):
+        assert not is_http_domain('griddynamics.com')
