f1fd3367bba94aeeee17137cf57b8ae976cc2f3f
tsalmon2 <tsalmon@griddynamics.com>
2023-03-22 10:55:38 -0500

    Completed test_task_exceptions

--- practice/3_python_testing/test_task_exceptions.py
+++ practice/3_python_testing/test_task_exceptions.py
@@ -7,15 +7,36 @@ If other cases it should check if division is correct
 TIP: to test output of print() function use capfd fixture
 https://stackoverflow.com/a/20507769
 """
+import pytest 
+from python_part_2 import task_exceptions as task
 
+class TestExceptions:
+    # Testing division with valid integers returns correct result
+    def test_division_ok(self):
+        assert task.division(18,2) == 9
 
-def test_division_ok(capfd):
-    ...
+    # Testing division by 0 returns None
+    def test_division_by_zero_return(self):
+        assert task.division(18,0) is None
 
+    # Testing division by 1 raises custom DivisionByOneException
+    def test_division_by_one(self):
+        with pytest.raises(task.DivisionByOneException):
+            task.division(18,1)
 
-def test_division_by_zero(capfd):
-    ...
-
-
-def test_division_by_one(capfd):
-    ...
+    # Testing division by valid integer or 1 prints "Division finished"
+    @pytest.mark.parametrize("callable", [(18,2), (18,1)])
+    def test_division_output(self, capfd, callable):
+        try:
+            task.division(*callable)
+        except(task.DivisionByOneException):
+            pass
+        finally:
+            captured = capfd.readouterr()
+            assert captured.out == "Division finished\n"
+        
+    # Testing division by 0 prints "Division by 0" and "Division finished"
+    def test_division_by_zero_stmt(self, capfd):
+        task.division(18,0)
+        captured = capfd.readouterr()
+        assert captured.out == "Division by 0\nDivision finished\n"
